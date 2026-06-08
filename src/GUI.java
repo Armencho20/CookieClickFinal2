402da3ba068cb4461df34452416f154fe5ed2223
ArmenOgannisyan <armen.ogannisyan@ht-la.org>
2026-06-08 00:39:34 -0700

    big boy changes

--- src/GUI.java
+++ src/GUI.java
@@ -3,109 +3,187 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/*
-TODO:
-Images?
-More upgrades that are unique
-Move the cookie button to the middle
-Add grandmas somewhere
-Make upgrades unlockable based on previous upgrades
-
-HOMEWORK:
-Figure out how to move the text panel directly below the button panel
-
- */
-
-
-
-
-
-
 public class GUI implements ActionListener {
+
     JFrame frame;
-    JPanel buttonPanel;
-    JPanel textPanel;
-    JPanel fillerPanel;
-    JLabel label;
-    JLabel upgrade1Cost;
-    JLabel upgrade2Cost;
+    JPanel panel;
+
     JButton cookieButton;
-    JButton upgradeButton1;
-    JButton upgradeButton2;
-    JButton SuperUpgradeButton;
+    JButton button1;
+    JButton button2;
+    JButton button3;
+    JButton button4;
+    JButton button5;
+    JButton rebirthButton;
+
+    JLabel cookieLabel;
+    JLabel label1;
+    JLabel label2;
+    JLabel label3;
+    JLabel label4;
+    JLabel label5;
+    JLabel rebirthLabel;
+
     Game game;
-    ActionListener guiUpdate;
     Timer updateTimer;
+
 
     public GUI() {
         game = new Game();
-        frame = new JFrame();//OUR WINDOW
-        buttonPanel = new JPanel();
-        textPanel = new JPanel();
-        fillerPanel = new JPanel();
-        label = new JLabel("Cookies: ");
-        upgrade1Cost = new JLabel("Upgrade Cost: " + game.upgrade1Cost);
-        upgrade2Cost = new JLabel("Upgrade Cost: " + game.upgrade2Cost);
+        frame = new JFrame();
+        panel = new JPanel();
+
         cookieButton = new JButton("Cookie");
-        upgradeButton1 = new JButton("Upgrade");
-        upgradeButton2 = new JButton("Slow Cooker");
-        SuperUpgradeButton = new JButton("Super Upgrade");
+        button1 = new JButton("Upgrade Click");
+        button2 = new JButton("Discount");
+        button3 = new JButton("2x Cookies (30s)");
+        button4 = new JButton("Grandma");
+        button5 = new JButton("Super Grandma");
+        rebirthButton = new JButton("Rebirth");
+
+
+        cookieLabel = new JLabel("Cookies: 0");
+        label1 = new JLabel("Cost: " + game.cost1 + " | Owned: 0");
+        label2 = new JLabel("Cost: " + game.cost2);
+        label3 = new JLabel("Cost: " + game.cost3 + " | Owned: 0");
+        label4 = new JLabel("Cost: " + game.cost4 + " | Owned: 0");
+        label5 = new JLabel("Cost: " + game.cost5 + " | Owned: 0");
+        rebirthLabel = new JLabel("Cost: " + game.rebirthCost + " | Rebirths: 0");
+
+        Font font = new Font("Verdana", Font.BOLD, 14);
+
+        cookieLabel.setFont(font);
+        cookieLabel.setForeground(new Color(120, 60, 20));
+
+        label1.setFont(font);
+        label1.setForeground(new Color(30, 70, 140));
+
+        label2.setFont(font);
+        label2.setForeground(new Color(30, 110, 50));
+
+        label3.setFont(font);
+        label3.setForeground(new Color(180, 90, 20));
+
+        label4.setFont(font);
+        label4.setForeground(new Color(90, 40, 130));
+
+        label5.setFont(font);
+        label5.setForeground(new Color(20, 110, 110));
+
+        rebirthLabel.setFont(font);
+        rebirthLabel.setForeground(new Color(150, 30, 30));
+
+        ImageIcon cookieIcon = new ImageIcon(getClass().getResource("/cookie.png"));
+        Image scaled = cookieIcon.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+        cookieButton.setIcon(new ImageIcon(scaled));
+
+
+        cookieButton.setFont(font);
+        cookieButton.setBackground(new Color(160, 90, 40));
+        cookieButton.setForeground(new Color(255, 230, 120));
+        cookieButton.setOpaque(true);
+        cookieButton.setBorderPainted(false);
+
+        button1.setFont(font);
+        button1.setBackground(new Color(40, 90, 160));
+        button1.setForeground(new Color(180, 240, 255));
+        button1.setOpaque(true);
+        button1.setBorderPainted(false);
+
+
+        button2.setFont(font);
+        button2.setBackground(new Color(40, 140, 70));
+        button2.setForeground(new Color(230, 255, 180));
+        button2.setOpaque(true);
+        button2.setBorderPainted(false);
+
+        button3.setFont(font);
+        button3.setBackground(new Color(210, 110, 30));
+        button3.setForeground(new Color(255, 255, 255));
+        button3.setOpaque(true);
+        button3.setBorderPainted(false);
+
+        button4.setFont(font);
+        button4.setBackground(new Color(110, 50, 150));
+        button4.setForeground(new Color(255, 200, 240));
+        button4.setOpaque(true);
+        button4.setBorderPainted(false);
+
+
+        button5.setFont(font);
+        button5.setBackground(new Color(30, 130, 130));
+        button5.setForeground(new Color(200, 255, 245));
+        button5.setOpaque(true);
+        button5.setBorderPainted(false);
+
+        rebirthButton.setFont(font);
+        rebirthButton.setBackground(new Color(170, 40, 40));
+        rebirthButton.setForeground(new Color(255, 220, 120));
+        rebirthButton.setOpaque(true);
+        rebirthButton.setBorderPainted(false);
+
 
         cookieButton.addActionListener(e -> {
             game.cookieClick();
-            label.setText("Cookies: " + game.cookieCounter);
+            cookieLabel.setText("Cookies: " + game.cookieCounter);
         });
-        upgradeButton1.addActionListener(e -> game.upgradeButton1());
-        upgradeButton2.addActionListener(e -> game.upgradeButton2());
 
-        //panel elements
-        buttonPanel.add(cookieButton);
-        buttonPanel.add(upgradeButton1);
-        buttonPanel.add(upgradeButton2);
-        textPanel.add(label);
-        textPanel.add(upgrade1Cost);
-        textPanel.add(upgrade2Cost);
-        //panel properties
-        buttonPanel.setSize(400, 100);
-        buttonPanel.setBackground(Color.PINK);
-        buttonPanel.setLayout(new GridLayout(1,4));
-        textPanel.setSize(400, 100);
-        textPanel.setBackground(Color.PINK);
-        textPanel.setLayout(new GridLayout(1,4));
-        fillerPanel.setBackground(Color.PINK);
+        button1.addActionListener(e -> game.buy1());
+        button2.addActionListener(e -> game.buy2());
+        button3.addActionListener(e -> game.buy3());
+        button4.addActionListener(e -> game.buy4());
+        button5.addActionListener(e -> game.buy5());
+        rebirthButton.addActionListener(e -> game.rebirth());
 
 
-        frame.add(buttonPanel,BorderLayout.NORTH);
-        fillerPanel.add(textPanel,BorderLayout.NORTH);
-        frame.add(fillerPanel,BorderLayout.CENTER);
-        //frame properties
-        frame.setBackground(Color.BLACK);
+        panel.setLayout(new GridLayout(7, 2, 5, 5));
+        panel.setBackground(Color.PINK);
+
+        panel.add(cookieButton);
+        panel.add(cookieLabel);
+        panel.add(button1);
+        panel.add(label1);
+        panel.add(button2);
+        panel.add(label2);
+        panel.add(button3);
+        panel.add(label3);
+        panel.add(button4);
+        panel.add(label4);
+        panel.add(button5);
+        panel.add(label5);
+        panel.add(rebirthButton);
+        panel.add(rebirthLabel);
+
+
+        frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400,400);
+        frame.setSize(400, 450);
         frame.setVisible(true);
 
-        //timer go here
-        guiUpdate = new ActionListener(){
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                label.setText("Cookies: " + game.cookieCounter);
-                upgrade1Cost.setText("Upgrade Cost: " + game.upgrade1Cost);
-                upgrade2Cost.setText("Upgrade Cost: " + game.upgrade2Cost);
-                if (game.cookieCounter >= 200){
-                    buttonPanel.add(SuperUpgradeButton);
-                }
-                //figure out the name of the label
-                //set the text of the label to something
-            }
-        };
-        updateTimer = new Timer(250,guiUpdate);
+        updateTimer = new Timer(250, this);
         updateTimer.start();
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        game.tickPassive();
 
+        cookieLabel.setText("Cookies: " + game.cookieCounter);
+        label1.setText("Cost: " + game.cost1 + " | Owned: " + game.count1);
+
+        if (game.discountUsed) {
+            label2.setText("Used");
+        } else {
+            label2.setText("Cost: " + game.cost2);
+        }
+
+        label3.setText("Cost: " + game.cost3 + " | Owned: " + game.count3);
+        label4.setText("Cost: " + game.cost4 + " | Owned: " + game.count4);
+        label5.setText("Cost: " + game.cost5 + " | Owned: " + game.count5);
+
+        rebirthLabel.setText("Cost: " + game.rebirthCost + " | Rebirths: " + game.rebirthCount);
     }
 }
+
+
--- src/Game.java
+++ src/Game.java
@@ -1,64 +1,153 @@
-import java.util.Timer;
-import java.util.TimerTask;
-/*
-TODO:
-Make the numbers increase when we buy more upgrades
-
-
- */
-
-
 public class Game {
-    protected int cookieCounter;
-    private int cookiesPerClick;
-    private int cookiesPerSecond;
-    protected int upgrade1Cost;
-    protected int upgrade2Cost;
-    private int upgradeCount;
 
-    public Game () {
+    protected int cookieCounter;
+    protected int cookiesPerClick;
+    protected int cookiesPerSecond;
+
+    protected int cost1;
+    protected int cost2;
+    protected int cost3;
+    protected int cost4;
+    protected int cost5;
+
+    protected int count1;
+    protected int count3;
+    protected int count4;
+    protected int count5;
+
+    protected int rebirthCount;
+    protected int rebirthCost;
+    protected int clickMultiplier;
+
+    protected boolean discountUsed;
+    protected int boostTicks;
+    private float passiveAccumulator;
+
+
+    public Game() {
         cookieCounter = 0;
         cookiesPerClick = 1;
         cookiesPerSecond = 0;
-        upgrade1Cost = 5;
-        upgrade2Cost = 25;
-        upgradeCount = 0;
+
+        cost1 = 10;
+        cost2 = 50;
+        cost3 = 100;
+        cost4 = 75;
+        cost5 = 300;
+
+        count1 = 0;
+        count3 = 0;
+        count4 = 0;
+        count5 = 0;
+
+        rebirthCount = 0;
+        rebirthCost = 10000;
+        clickMultiplier = 1;
+
+        discountUsed = false;
+        boostTicks = 0;
+        passiveAccumulator = 0f;
+    }
 
 
-        Timer slowCooker = new Timer();
-        TimerTask slow = new TimerTask() {
-            @Override
-            public void run() {
-                cookieCounter+=cookiesPerSecond;
-            }
-        };
-        slowCooker.schedule(slow,0,1000);
+    public void tickPassive() {
+        if (boostTicks > 0) {
+            boostTicks--;
+        }
 
+        passiveAccumulator += cookiesPerSecond / 4f;
 
-
-
+        if (passiveAccumulator >= 1f) {
+            cookieCounter += (int) passiveAccumulator;
+            passiveAccumulator -= (int) passiveAccumulator;
+        }
     }
 
     public void cookieClick() {
-        cookieCounter+=cookiesPerClick;
+        int amount = cookiesPerClick * clickMultiplier;
+
+        if (boostTicks > 0) {
+            amount *= 2;
+        }
+
+        cookieCounter += amount;
     }
-    public void upgradeButton1() {
-        if (cookieCounter >= upgrade1Cost ) {
+
+
+    public void buy1() {
+        if (cookieCounter >= cost1) {
+            cookieCounter -= cost1;
             cookiesPerClick++;
-            cookieCounter -= upgrade1Cost;
-            upgradeCount++;
-            upgrade1Cost += upgradeCount;
+            count1++;
+            cost1 += 10;
         }
-
-    }
-    public void upgradeButton2() {
-        if (cookieCounter >= upgrade2Cost ) {
-            cookiesPerSecond++;
-            cookieCounter -= upgrade2Cost;
-            //
-            //
-        }
-
     }
 
+    public void buy2() {
+        if (cookieCounter >= cost2 && !discountUsed) {
+            cookieCounter -= cost2;
+            cost1 = cost1 / 2;
+            cost3 = cost3 / 2;
+            cost4 = cost4 / 2;
+            cost5 = cost5 / 2;
+            discountUsed = true;
+        }
+    }
+
+    public void buy3() {
+        if (cookieCounter >= cost3) {
+            cookieCounter -= cost3;
+            boostTicks = 120;
+            count3++;
+            cost3 += 100;
+        }
+    }
+
+
+    public void buy4() {
+        if (cookieCounter >= cost4) {
+            cookieCounter -= cost4;
+            cookiesPerSecond += 1;
+            count4++;
+            cost4 += 25;
+        }
+    }
+
+    public void buy5() {
+        if (cookieCounter >= cost5) {
+            cookieCounter -= cost5;
+            cookiesPerSecond += 5;
+            count5++;
+            cost5 += 150;
+        }
+    }
+
+    public void rebirth() {
+        if (cookieCounter >= rebirthCost) {
+            rebirthCount++;
+            rebirthCost *= 2;
+            clickMultiplier *= 2;
+
+            cookieCounter = 0;
+            cookiesPerClick = 1;
+            cookiesPerSecond = 0;
+
+            cost1 = 10;
+            cost2 = 50;
+            cost3 = 100;
+            cost4 = 75;
+            cost5 = 300;
+
+            count1 = 0;
+            count3 = 0;
+            count4 = 0;
+            count5 = 0;
+
+            discountUsed = false;
+            boostTicks = 0;
+            passiveAccumulator = 0f;
+        }
+    }
 }
+
+
